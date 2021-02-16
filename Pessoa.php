e35428b18545b68104dd2f86394ba8bc5beb01b0
Afonso-cmd <afonso.ur@gmail.com>
2021-02-16 02:36:53 -0300

    Organizando Arquivos

---
+++ Pessoa.php
@@ -0,0 +1,16 @@
+<?php  
+
+class Pessoa{ //criando classe
+	public $nome;//atributos
+	public $idade;
+
+
+	public function falarNome(){//métodos
+		echo $this->nome;
+	}
+
+		public function falarIdade(){//métodos
+		echo $this->idade;
+	}
+
+}
--- index.php
+++ index.php
@@ -1,24 +1,12 @@
 <?php  
 
-class Pessoa{ //criando classe
-	public $nome;//atributos
-	public $idade;
-
-
-	public function falarNome(){//métodos
-		echo $this->nome;
-	}
-
-		public function falarIdade(){//métodos
-		echo $this->idade;
-	}
-
-}
+require "Pessoa.php";//trazer a classe php   require_once   também   once(ser usado uma ínica vez)
+//pode-se usar o include também
 
 $uma_pessoa = new Pessoa;//instância 
 $uma_pessoa->nome = "Afonso";
 $uma_pessoa->idade  = "27";
 
-var_dump($uma_pessoa);// impressão da classe 
 
 $uma_pessoa->falarNome(); // impressão pela chamada do método
+$uma_pessoa->falarIdade();
